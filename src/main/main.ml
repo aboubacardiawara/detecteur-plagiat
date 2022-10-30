a4226e125d4d802fad109ff71bd1a9aa6c307dfa
diawara <aboubacar.diawara.etu@univ-lille.fr>
2022-10-30 17:50:27 +0100

    [ref problem] the matrix rows elements have the same reference

--- src/main/main.ml
+++ src/main/main.ml
@@ -11,7 +11,14 @@ let col_count matrix = Array.length (Array.get !matrix 0)
 let from_string s1 s2 = 
 let l = ref (
  let n=String.length s1 and m=String.length s2 in 
-  Array.make_matrix n m (ref 0)) in l;;  
+  Array.make_matrix n m (ref 0)) in l;;
+
+let new_matrix s1 s2 = 
+  let l = ref (
+ let n=String.length s1 and m=String.length s2 in 
+  Array.init n ( fun _ -> Array.init m (fun _ -> ref 0))) in l;;
+
+let create_matrix = new_matrix
 
 let max_length matrix =
  let max_size = ref 0 in  
@@ -21,27 +28,44 @@ let max_length matrix =
    done
   done;
   max_size
-let value_to_set s1 s2 matrix i j =
-  5 (**to do *) 
+  
+let value_to_set_basic s1 s2 matrix i j =
+  if (String.get s1 i = String.get s2 j) then 
+    if i=0 || j=0 then 1
+    else !(get matrix (i-1) (j-1)) + 1
+  else 0 
+
+let value_to_set_decorated s1 s2 matrix i j =
+  print_string (string_of_bool(String.get s1 i = String.get s2 j));
+  print_string " ";
+  print_int i;
+  print_int j;
+  print_newline ();
+  value_to_set_basic s1 s2 matrix i j
+
+let value_to_set = value_to_set_basic
 
 let fill_first_row s1 s2 matrix = 
   for col=0 to (col_count matrix-1) do
-    (get matrix 0 col) := value_to_set s1 s2 matrix 0 col
+    set matrix 0 col (value_to_set s1 s2 matrix 0 col)
   done
 
 let fill_first_col s1 s2 matrix = 
   for row=0 to (row_count matrix-1) do
-    (get matrix row 0) := value_to_set s1 s2 matrix row 0
+    set matrix row 0 (value_to_set s1 s2 matrix row 0)
   done
 
 let compute_remaining_cell s1 s2 matrix =
-    fill_first_row s1 s2 matrix;
-    fill_first_col s1 s2 matrix;
     for row=0 to ( String.length s1 -1) do
       for col=0 to (String.length s2 -1) do
         (get matrix row 0) := value_to_set s1 s2 matrix row col
       done
     done
+
+let fill_cell s1 s2 matrix =
+    fill_first_row s1 s2 matrix;
+    fill_first_col s1 s2 matrix;
+    compute_remaining_cell s1 s2 matrix
 
 let display matrix = 
   print_string "[";
@@ -55,4 +79,6 @@ let display matrix =
   done;
   print_string "\n]"
 
-let () = let s1="abou" and s2="babacar" in let m=(from_string s1 s2) in compute_remaining_cell s1 s2 m;  display m
+let () = let s1="abour" and s2="babacar" in
+           let m=(create_matrix s1 s2) in 
+            (fill_cell s1 s2 m; display m)
