b2cc74c25fb909db97a5c4d31164ffbf9934d305
diawara <aboubacar.diawara.etu@univ-lille.fr>
2022-11-13 15:44:40 +0100

    ajout d'information aux noeud pour determiner la plus longue sous chaine commune

--- src/main/suffixe_tree.ml
+++ src/main/suffixe_tree.ml
@@ -3,14 +3,40 @@
     Autors: 
     Date:
 *)
+exception EmptyOption
+exception TestFailure
 
-type noeud_lex = Lettre of char * arbre_lex
+type option = None | Occurences of int*int;;
+
+type noeud_lex = Lettre of string * option * arbre_lex
 and arbre_lex = noeud_lex  list;;
 
+(*primitives on option*)
+let defaultOption = None
+
+let fst = function 
+  None -> raise EmptyOption
+  | Occurences (a, _) -> a 
+
+let snd = function
+None -> raise EmptyOption
+| Occurences (_, b) -> b 
+
+let isEmpty = function
+  None -> true
+  | _ -> false
+
+
 (* HELPERS *)
-let consume s = 
+let consumeNextChar s = 
   let a=String.get s 0 and b=String.sub s 1 ((String.length s)-1) in
   (a, b)
+
+let consumeNextStr s = 
+  let a=String.sub s 0 1 and b=String.sub s 1 ((String.length s)-1) in 
+    (a, b)
+
+let consume = consumeNextStr
 
 let rec substrings (w:string): string list = 
   if w="" then []
@@ -20,7 +46,7 @@ let rec substrings (w:string): string list =
 let rec sorted_insertion node tree = 
   match node,tree with
     _, [] -> [node]
-    | Lettre (v, _), (Lettre (v', _) as node')::ns ->
+    | Lettre (v, opt1, _), (Lettre (v', opt2, _) as node')::ns ->
         if v < v' then node :: tree
         else node' :: (sorted_insertion node ns)
 
@@ -30,16 +56,16 @@ let rec sorted_insertion node tree =
  ***)
 let rec size = function 
     [] -> 0
-  | Lettre (v, fils) :: ns -> 
-      if v = '#' then 0 + (size ns)
+  | Lettre (v, opt, fils) :: ns -> let isTerminal = fun s -> s="#" || s="$" in
+      if isTerminal v then 0 + (size ns)
       else 1 + (size fils) + (size ns) 
 
 let rec ajoute(m:string) (a:arbre_lex): arbre_lex =
   match m,a with
     "",_ -> a
-    |_, []->let c,cs = consume m in [Lettre (c, ajoute cs [])]
-    |_, (Lettre (v, fils) as n)::ns -> let c,cs= consume m in
-      if c=v then (sorted_insertion (Lettre (v, ajoute cs fils)) ns)
+    |_, []->let c,cs = consume m in [Lettre (c, defaultOption, ajoute cs [])]
+    |_, (Lettre (v, option, fils) as n)::ns -> let c,cs= consume m in
+      if c=v then (sorted_insertion (Lettre (v, option, ajoute cs fils)) ns)
       else sorted_insertion n (ajoute m ns)   
 
 (*teste si un mot appartient à l'arbre*)
@@ -47,7 +73,7 @@ let rec existe (m:string) (a:arbre_lex): bool =
   match a,  m with
     [], "" | _, "" | _, "#" -> true
   | [], _ -> false
-  | Lettre (v, tree)::remaining_nodes, m -> 
+  | Lettre (v, _, tree)::remaining_nodes, m -> 
       let c, cs = (consume m) in
       if c=v then existe cs tree
       else existe m remaining_nodes
@@ -68,15 +94,44 @@ let arbreSuffixes = fun w -> construit (substrings w)
 (*is w2 substring of w1 ?*)
 let sousChaine w1 w2 = existe w2 (arbreSuffixes w1)
 
+let rec for_each_node tree f = match tree with
+  [] -> true
+  |(Lettre (v, option, sub) as x)::xs -> (f x) && (for_each_node sub f) && (for_each_node xs f)  
 
-(**Tests *)
+let selecte_longest (tree:arbre_lex): = __
+let counter (tree:arbre_lex):arbre_lex= __
+
+let sousChainesCommunes s1 s2 =
+  let tree=(arbreSuffixes (s1^"#")) in 
+    let tree' = ajoute (s2 ^"$") tree in
+      selecte_longest (counter tree')
+
+
+
+
+(**
+ TESTS 
+*)
+let treeExample = arbreSuffixes "ananans#banane$"
+
 let rec all_couple_are_sousChaine = function
-      [] -> true
-      | (w1, w2)::examples -> sousChaine w1 w2 && (work_well_sousChaine examples) 
+    [] -> true
+    | (w1, w2)::examples -> sousChaine w1 w2 && (all_couple_are_sousChaine examples)
     
 let testSousChaine = 
   let l = [("ananas#", "anas"); ("ananas#", "anas#"); ("ananas#", "#"); ("ananas#", "anas")]
   and l' = [("anas", "ane"); ("hello world", "ello wolrd")] in
   all_couple_are_sousChaine l && (not (all_couple_are_sousChaine l'))
-  
-let () =  if testSousChaine then print_string ":)" else print_string ":("
+
+
+let get_node_option = function
+  Lettre (v, option, _) -> option
+
+let testNodesOptionAreInitiallyEmpty =
+  let tree = treeExample in 
+    for_each_node tree (fun node -> 
+      if (get_node_option node) = None then true
+      else false)
+
+
+let () =  if testNodesOptionAreInitiallyEmpty then print_string ":)" else print_string ":("
